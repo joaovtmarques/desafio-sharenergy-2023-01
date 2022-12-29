9333fbf12783f3230f0d8267e05734b1cd8aa163
João Vitor Marques <jvsilvam@outlook.com>
2022-12-29 13:26:24 -0300

    feat: create models

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,9 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-import express, { Request, Response } from 'express';
+import express from 'express';
 
 const app = express();
 app.use(express.json());
-
-app.get('/test', (req: Request, res: Response) => {
-  return res.json({ ok: true });
-});
 
 app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`));
