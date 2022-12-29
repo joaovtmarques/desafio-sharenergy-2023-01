49fcdc62ce172d3e467b634f3229d270b1e32da1
João Vitor Marques <jvsilvam@outlook.com>
2022-12-29 10:38:48 -0300

    chore: add pre-push hook

---
+++ server/src/index.spec.ts
@@ -0,0 +1,9 @@
+import { Any } from '.';
+
+describe('', () => {
+  test('', () => {
+    const any = new Any();
+
+    expect(any._any()).toEqual('Any');
+  });
+});
---
+++ server/src/index.ts
@@ -0,0 +1,5 @@
+export class Any {
+  _any() {
+    return 'Any';
+  }
+}
