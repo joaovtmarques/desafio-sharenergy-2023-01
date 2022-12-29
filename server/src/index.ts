4f61a672a0df1e25f5ad59bbe2304d1ea097f342
João Vitor Marques <jvsilvam@outlook.com>
2022-12-29 12:33:43 -0300

    chore: setup express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,14 @@
 import * as dotenv from 'dotenv';
 
 dotenv.config();
+
+import express, { Request, Response } from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.get('/test', (req: Request, res: Response) => {
+  return res.json({ ok: true });
+});
+
+app.listen(process.env.PORT, () => console.log(`Server started on port ${process.env.PORT}`));
