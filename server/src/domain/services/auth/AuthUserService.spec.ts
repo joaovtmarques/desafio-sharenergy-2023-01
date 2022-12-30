ed00b6357b483f5e78a43210da9adf6327b11554
João Vitor Marques <jvsilvam@outlook.com>
2022-12-30 12:13:10 -0300

    test: generate new token based on a refresh token

--- server/src/domain/services/auth/AuthUserService.spec.ts
+++ server/src/domain/services/auth/AuthUserService.spec.ts
@@ -3,6 +3,7 @@ import { HttpStatusCode } from '@/src/shared/types/httpModel';
 import { InMemoryUserRepository } from '@/test/repositories/inMemoryUserRepository';
 import { CreateUserService } from './../user/CreateUserService';
 import { AuthUserService } from './AuthUserService';
+import { RefreshTokenUserService } from './RefreshTokenUserSevice';
 
 describe('Authenticate an user', () => {
   it('should authenticate an user', async () => {
@@ -84,6 +85,37 @@ describe('Authenticate an user', () => {
       expect.objectContaining({
         token: expect.any(String),
         refreshToken: expect.any(Object),
+      })
+    );
+  });
+
+  it('should return a new token based on a refresh token', async () => {
+    const inMemoryUserRepository = new InMemoryUserRepository();
+    const authUserService = new AuthUserService(inMemoryUserRepository);
+    const createUserService = new CreateUserService(inMemoryUserRepository);
+    const refreshTokenService = new RefreshTokenUserService(inMemoryUserRepository);
+
+    const data = {
+      email: '_any@email.com',
+      password: '_anypassword',
+    };
+
+    await createUserService.execute(data);
+
+    const token = await authUserService.execute({ email: data.email, password: data.password });
+
+    expect(token).toEqual(
+      expect.objectContaining({
+        token: expect.any(String),
+        refreshToken: expect.any(Object),
+      })
+    );
+
+    const newToken = await refreshTokenService.execute(token.refreshToken.id);
+
+    expect(newToken).toEqual(
+      expect.objectContaining({
+        token: expect.any(String),
       })
     );
   });
