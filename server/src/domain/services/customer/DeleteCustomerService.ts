5f215742a73ef181f172609102a61872799252bb
João Vitor Marques <jvsilvam@outlook.com>
2022-12-31 10:51:28 -0300

    feat: return error when customer to delete is not found

--- server/src/domain/services/customer/DeleteCustomerService.ts
+++ server/src/domain/services/customer/DeleteCustomerService.ts
@@ -1,10 +1,20 @@
 import { CustomerRepository } from '@/src/infra/repositories';
+import { BaseError } from '@/src/shared/classes/baseError';
+import { HttpStatusCode } from '@/src/shared/types/httpModel';
+
+import { FindCustomerByIdService } from './FindCustomerByIdService';
 
 export class DeleteCustomerService {
   // eslint-disable-next-line no-unused-vars
   constructor(private customerRepository: CustomerRepository) {}
 
   async execute(customerId: string): Promise<void> {
-    await this.customerRepository.delete(customerId);
+    const findCustomerByIdService = new FindCustomerByIdService(this.customerRepository);
+    const customer = await findCustomerByIdService.execute(customerId);
+
+    if (!customer) {
+      throw new BaseError('Customer not found', 'deleteCustomer', HttpStatusCode.NOT_FOUND);
+    }
+    await this.customerRepository.delete(customer.id);
   }
 }
