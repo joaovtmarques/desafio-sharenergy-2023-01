3138bdb07da4a8201449d29d337837de8209f631
João Vitor Marques <jvsilvam@outlook.com>
2022-12-31 11:16:42 -0300

    refactor: find customer on delete customer

--- server/src/domain/services/customer/DeleteCustomerService.ts
+++ server/src/domain/services/customer/DeleteCustomerService.ts
@@ -2,15 +2,12 @@ import { CustomerRepository } from '@/src/infra/repositories';
 import { BaseError } from '@/src/shared/classes/baseError';
 import { HttpStatusCode } from '@/src/shared/types/httpModel';
 
-import { FindCustomerByIdService } from './FindCustomerByIdService';
-
 export class DeleteCustomerService {
   // eslint-disable-next-line no-unused-vars
   constructor(private customerRepository: CustomerRepository) {}
 
   async execute(customerId: string): Promise<void> {
-    const findCustomerByIdService = new FindCustomerByIdService(this.customerRepository);
-    const customer = await findCustomerByIdService.execute(customerId);
+    const customer = await this.customerRepository.findById(customerId);
 
     if (!customer) {
       throw new BaseError('Customer not found', 'deleteCustomer', HttpStatusCode.NOT_FOUND);
