4d8f48c9da80b521d7a944d047e6e5134db32d6e
João Vitor Marques <jvsilvam@outlook.com>
2023-01-02 10:45:19 -0300

    feat: user not found exception

--- server/src/shared/classes/baseError.ts
+++ server/src/shared/classes/baseError.ts
@@ -2,9 +2,9 @@ import { HttpStatusCode } from '../types/httpStatusCode';
 
 export class BaseError extends Error {
   methodName: string;
-  httpCode: HttpStatusCode;
+  httpCode?: HttpStatusCode;
 
-  constructor(message: string, methodName: string, httpCode: HttpStatusCode) {
+  constructor(message: string, methodName: string, httpCode?: HttpStatusCode) {
     super(message);
 
     this.message = message;
