37f99dd37a33f2eab455e035112d5677b7a64cbd
João Vitor Marques <jvsilvam@outlook.com>
2023-01-02 20:36:21 -0300

    test: return error when email or password to login is incorrect

--- server/src/api/controllers/auth/test/AuthUserController.spec.ts
+++ server/src/api/controllers/auth/test/AuthUserController.spec.ts
@@ -32,4 +32,15 @@ describe('Authenticate an user', () => {
       })
     );
   });
+
+  it('should return error when email or password is incorrect', async () => {
+    const response = await supertest(app)
+      .post('/login')
+      .send({ email: '_anyincorrect@email.com', password: '_anyincorrectpass' });
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toEqual('Incorrect email or password');
+    expect(response.body.method).toEqual('authenticateUser');
+    expect(response.body.statusCode).toEqual(400);
+  });
 });
