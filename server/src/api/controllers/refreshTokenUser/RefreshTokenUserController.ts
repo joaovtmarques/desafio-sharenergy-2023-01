b14e2cbe2b630e9a96870647db91283dfcc49594
João Vitor Marques <jvsilvam@outlook.com>
2023-01-02 21:18:55 -0300

    feat: return error when refresh token is invalid

--- server/src/api/controllers/refreshTokenUser/RefreshTokenUserController.ts
+++ server/src/api/controllers/refreshTokenUser/RefreshTokenUserController.ts
@@ -1,14 +1,19 @@
 import { RefreshTokenUserService } from '@/src/domain/services/auth/RefreshTokenUserSevice';
+import { BaseError } from '@/src/shared/classes/baseError';
 import { HttpRequest } from '@/src/shared/types/httpRequest';
 
 export class RefreshTokenUserController {
   constructor(private refreshTokenUserService: RefreshTokenUserService) {}
 
   async handle({ req, res, next }: HttpRequest) {
-    const { refreshToken } = req.body;
+    try {
+      const { refreshToken } = req.body;
 
-    const token = await this.refreshTokenUserService.execute(refreshToken);
+      const token = await this.refreshTokenUserService.execute(refreshToken);
 
-    return res.status(200).send(token);
+      return res.status(200).send(token);
+    } catch (err) {
+      if (err instanceof BaseError) next(new BaseError(err.message, err.methodName, err.httpCode));
+    }
   }
 }
