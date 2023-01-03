5eee33d9b17642614ae871b438520433fd54b231
João Vitor Marques <jvsilvam@outlook.com>
2023-01-03 19:48:04 -0300

    feat: find customer by id route and controller

--- server/src/api/controllers/customer/index.ts
+++ server/src/api/controllers/customer/index.ts
@@ -1,4 +1,5 @@
 import { findAllCustomersFactory } from './factories/FindAllCustomersFactory';
 import { createCustomerFactory } from './factories/CreateCustomerFactory';
+import { findCustomerByIdFactory } from './factories/FindCustomerByIdFactory';
 
-export { createCustomerFactory, findAllCustomersFactory };
+export { createCustomerFactory, findAllCustomersFactory, findCustomerByIdFactory };
--- server/src/api/routes/CustomerRoutes.ts
+++ server/src/api/routes/CustomerRoutes.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express';
 
-import { createCustomerFactory, findAllCustomersFactory } from '../controllers/customer';
+import {
+  createCustomerFactory,
+  findAllCustomersFactory,
+  findCustomerByIdFactory,
+} from '../controllers/customer';
 
 const customer = Router();
 
 customer.post('/customers', (req, res, next) => createCustomerFactory().handle({ req, res, next }));
 customer.get('/customers', (req, res, next) =>
   findAllCustomersFactory().handle({ req, res, next })
+);
+customer.get('/customers/:id', (req, res, next) =>
+  findCustomerByIdFactory().handle({ req, res, next })
 );
 
 export { customer };
