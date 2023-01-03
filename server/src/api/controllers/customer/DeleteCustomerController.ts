486d84e1fabac0f9535119943599d3f5c63a730d
João Vitor Marques <jvsilvam@outlook.com>
2023-01-03 20:15:10 -0300

    feat: return error when customer to delete is not found

--- server/src/api/controllers/customer/DeleteCustomerController.ts
+++ server/src/api/controllers/customer/DeleteCustomerController.ts
@@ -1,5 +1,8 @@
 import { DeleteCustomerService } from '@/src/domain/services/customer/DeleteCustomerService';
+import { BaseError } from '@/src/shared/classes/baseError';
 import { HttpRequest } from '@/src/shared/types/httpRequest';
+import { HttpStatusCode } from '@/src/shared/types/httpStatusCode';
+import { CustomerNotFoundException } from '../../exceptions/CustomerNotFoundException';
 
 export class DeleteCustomerController {
   constructor(private deleteCustomerService: DeleteCustomerService) {}
@@ -7,8 +10,13 @@ export class DeleteCustomerController {
   async handle({ req, res, next }: HttpRequest) {
     const { id } = req.params;
 
-    await this.deleteCustomerService.execute(id);
+    try {
+      await this.deleteCustomerService.execute(id);
 
-    return res.status(200).send();
+      return res.status(HttpStatusCode.OK).send();
+    } catch (err) {
+      if (err instanceof BaseError)
+        return next(new CustomerNotFoundException(`Customer {${id}} not found`, err.methodName));
+    }
   }
 }
