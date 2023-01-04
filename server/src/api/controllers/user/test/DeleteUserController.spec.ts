ab97b0571350ff325c48f0ea303a924b89753662
João Vitor Marques <jvsilvam@outlook.com>
2023-01-04 13:15:08 -0300

    refactor: add auth validation to user constroller tests

--- server/src/api/controllers/user/test/DeleteUserController.spec.ts
+++ server/src/api/controllers/user/test/DeleteUserController.spec.ts
@@ -1,18 +1,41 @@
 import { app } from '@/src/app';
-import request from 'supertest';
+import supertest from 'supertest';
 
-describe.skip('Delete user', () => {
+describe('Delete user', () => {
+  let userResponse: supertest.Response;
+  let token: supertest.Response;
+
+  beforeEach(async () => {
+    userResponse = await supertest(app)
+      .post('/users')
+      .send({ email: '_any@email.com.br', password: '_anypassword' });
+
+    token = await supertest(app)
+      .post('/login')
+      .send({ email: '_any@email.com.br', password: '_anypassword' });
+  });
+
+  afterEach(async () => {
+    await supertest(app).delete(`/users/${userResponse.body.id}`).send();
+  });
+
   it('should delete an user', async () => {
     const data = {
       email: '_any@email.com',
       password: '_anypass',
     };
 
-    const user = await request(app).post('/users').send(data);
+    const user = await supertest(app).post('/users').send(data);
 
-    await request(app).delete(`/users/${user.body.id}`).send();
+    await supertest(app)
+      .delete(`/users/${user.body.id}`)
+      .auth(token.body.token, { type: 'bearer' })
+      .send();
 
-    const userExists = await request(app).get(`/users/${user.body.id}`).send();
+    const userExists = await supertest(app)
+      .get(`/users/${user.body.id}`)
+      .auth(token.body.token, { type: 'bearer' })
+      .send();
 
     expect(userExists.status).toBe(404);
     expect(userExists.body.message).toBe(`User {${user.body.id}} not found`);
@@ -21,7 +44,10 @@ describe.skip('Delete user', () => {
   it('should return error when user to delete not found', async () => {
     const userId = '_anyuserid';
 
-    const response = await request(app).delete(`/users/${userId}`).send();
+    const response = await supertest(app)
+      .delete(`/users/${userId}`)
+      .auth(token.body.token, { type: 'bearer' })
+      .send();
 
     expect(response.status).toBe(404);
     expect(response.body.message).toBe(`User {${userId}} not found`);
--- server/src/api/controllers/user/test/FindUserByIdController.spec.ts
+++ server/src/api/controllers/user/test/FindUserByIdController.spec.ts
@@ -1,16 +1,36 @@
 import { app } from '@/src/app';
-import request from 'supertest';
+import supertest from 'supertest';
 
-describe.skip('Find user by id', () => {
+describe('Find user by id', () => {
+  let userResponse: supertest.Response;
+  let token: supertest.Response;
+
+  beforeEach(async () => {
+    userResponse = await supertest(app)
+      .post('/users')
+      .send({ email: '_any@email.com.br', password: '_anypassword' });
+
+    token = await supertest(app)
+      .post('/login')
+      .send({ email: '_any@email.com.br', password: '_anypassword' });
+  });
+
+  afterEach(async () => {
+    await supertest(app).delete(`/users/${userResponse.body.id}`).send();
+  });
+
   it('should find user by id', async () => {
     const data = {
       email: '_any@email.com',
       password: '_anypass',
     };
 
-    const response = await request(app).post('/users').send(data);
+    const response = await supertest(app).post('/users').send(data);
 
-    const user = await request(app).get(`/users/${response.body.id}`).send();
+    const user = await supertest(app)
+      .get(`/users/${response.body.id}`)
+      .auth(token.body.token, { type: 'bearer' })
+      .send();
 
     expect(user.status).toBe(200);
     expect(user.body).toEqual(
